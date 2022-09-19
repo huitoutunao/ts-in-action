ac905e7d2147b59cee2265cf5a9681bf89462ebe
huitoutunao <lh_huitoutunao@163.com>
2022-09-19 23:47:15 +0800

    feat: 更新ts高级类型

--- src/advanced.ts
+++ src/advanced.ts
@@ -281,3 +281,28 @@ function area(s: Shape) {
   }
 }
 console.log(area({ kind: 'circle', r: 1 }))
+
+// 索引类型
+// keyof T
+interface Obj {
+  a: number;
+  b: string;
+}
+let key: keyof Obj
+
+// 索引访问操作符
+// T[K]
+let value: Obj['a']
+
+// T extends U
+
+let obj3 = {
+  a: 1,
+  b: 2,
+  c: 3,
+}
+function getValue<T, K extends keyof T>(obj: T, keys: K[]): T[K][] {
+  return keys.map((item) => obj[item])
+}
+console.log(getValue(obj3, ['a', 'b']))
+// console.log(getValue(obj3, ['e', 'f'])) // 不合法
