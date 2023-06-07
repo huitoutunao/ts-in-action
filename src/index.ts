d539dfbbb4d43b5594915976cf98b96314ca7dc6
huitoutunao <lh_huitoutunao@163.com>
2023-06-07 11:24:52 +0800

    feat: 整理文件

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+//#region
+// 这个是初识学习文件
 // import './data-type'
 // import './enum'
 // import './interface'
 // import './function'
 // import './class'
+//#endregion
+
+//#region
+// 这是复习学习文件
+// import ./review/data-type.ts
+//#endregion
 
 const demo: string = 'hello world'
 console.log('say', demo)
