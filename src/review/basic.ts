1b4637fbfb1c5c50c6f145887680a8245a5ded1a
huitoutunao <lh_huitoutunao@163.com>
2023-06-08 23:46:41 +0800

    feat: 新增基础类型定义

--- src/review/basic.ts
+++ src/review/basic.ts
@@ -81,4 +81,75 @@
     // return null // 类型报错
     // return undefined // 类型报错
   }
+
+  // object 表示一个 js 变量，一般不使用
+  let h: object
+  h = {}
+  h = function() {}
+
+  // {} 用来指定对象中可以包含哪些属性
+  // 语法：{属性名: 属性值, 属性名: 属性值}
+  // 在属性名后面加上 ?，表示属性是可选
+  let h2: { name: string, age?: number }
+  h2 = { name: '张三' }
+  h2 = { name: '张三', age: 18 }
+
+  // [propName: string]: any 表示任意类型的属性
+  let h3: { name: string, [propName: string]: any }
+  h3 = { name: '李四', age: 19, gender: '男' }
+
+  /**
+   * 设置函数结构的类型声明：
+   *  语法：(形参: 类型, 形参: 类型 ...) => 类型
+   */
+  let h4: (a: number, b: number) => number
+  h4 = function(n1, n2): number {
+    return n1 + n2
+  }
+
+  /**
+   * 设置数组类型
+   *  语法：类型[] 或者 Array<类型>
+   *
+  */
+  // string[] 表示字符串数组
+  let j: string[]
+  j = ['a', 'b', 'c']
+
+  let j2: number[]
+  j2 = [1, 2, 3]
+
+  let j3: Array<string>
+  j3 = ['1', '2', '3']
+
+  /**
+   * tuple 元组，元组就是固定长度的数组
+   * 语法：[类型，类型，类型 ...]
+  */
+  let k: [string, string]
+  k = ['1', '2']
+
+  /**
+   * enum 枚举
+  */
+  enum Gender {
+    Male = 0,
+    Female = 1
+  }
+  let l: { name: string, gender: Gender }
+  l = {
+    name: '张三',
+    gender: Gender.Male,
+  }
+  // console.log('枚举值', l.gender === Gender.Male)
+
+  // & 表示同时满足
+  let m: { name: string } & { age: number }
+  m = { name: '王五', age: 17 }
+
+  // 类型别名
+  type myType = 1 | 2 | 3
+  let n: myType
+  n = 1
+  // n = 4 // 类型报错
 })()
